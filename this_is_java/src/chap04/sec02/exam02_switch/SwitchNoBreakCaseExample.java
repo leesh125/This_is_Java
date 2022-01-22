32be353488979878d862d90ffc6d37ae86519731
leesh125 <chelsea12345@naver.com>
2022-01-22 23:52:54 +0900

    4장 조건문과 반복문 - 4.2 조건문

---
+++ this_is_java/src/chap04/sec02/exam02_switch/SwitchNoBreakCaseExample.java
@@ -0,0 +1,22 @@
+package chap04.sec02.exam02_switch;
+
+public class SwitchNoBreakCaseExample {
+
+	public static void main(String[] args) {
+		int time = (int)(Math.random()*4) + 8;
+		System.out.println("[현재 시간: " + time + "]");
+		
+		switch(time) {
+		  case 8:
+			  System.out.println("출근 합니다.");
+		  case 9:
+			  System.out.println("회의를 합니다.");
+		  case 10:
+			  System.out.println("업무를 봅니다.");
+		  default:
+			  System.out.println("외근을 나갑니다.");
+		}
+
+	}
+
+}
