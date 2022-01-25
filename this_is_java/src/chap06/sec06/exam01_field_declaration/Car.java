5f533f3890ba9fe62a0f1de6d003bcc8e52b97e7
leesh125 <chelsea12345@naver.com>
2022-01-25 23:12:31 +0900

    6장 클래스 - 6.6 필드

---
+++ this_is_java/src/chap06/sec06/exam01_field_declaration/Car.java
@@ -0,0 +1,10 @@
+package chap06.sec06.exam01_field_declaration;
+
+public class Car {
+	// 필드: 객체의 고유 데이터
+	String company = "현대자동차";
+	String model = "그랜저";
+	String color = "검정";
+	int maxSpeed = 350;
+	int speed; // 기본값 0
+}
---
+++ this_is_java/src/chap06/sec06/exam02_field_default_value/FieldInitValue.java
@@ -0,0 +1,18 @@
+package chap06.sec06.exam02_field_default_value;
+
+public class FieldInitValue {
+	// 필드
+	byte byteField;
+	short shortField;
+	int intField;
+	long longField; // 0
+	
+	boolean booleanField; // false
+	char charField; // 빈공백 ''
+	
+	float floatField; // 0.0
+	double doubleField; // 0.0
+	
+	int[] arrField; // null
+	String referenceField; // null
+}
