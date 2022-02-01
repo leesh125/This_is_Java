fd6cd6e56ef96e9779d57c746c91f8503b7211d6
leesh125 <chelsea12345@naver.com>
2022-02-01 19:18:20 +0900

    7장 상속 - 7.2 클래스 상속

---
+++ this_is_java/src/chap07/sec02/exam01_inheritance/DmbCellPhoneExample.java
@@ -0,0 +1,28 @@
+package chap07.sec02.exam01_inheritance;
+
+public class DmbCellPhoneExample {
+
+	public static void main(String[] args) {
+		DmbCellPhone dmbCellPhone = new DmbCellPhone("자바폰", "검정", 10);
+		
+		// 부모 필드 상속
+		System.out.println("모델: " + dmbCellPhone.model);
+		System.out.println("색상: " + dmbCellPhone.color);
+		
+		System.out.println("채널: " + dmbCellPhone.channel);
+		
+		// 부모 메소드 상속
+		dmbCellPhone.powerOn();
+		dmbCellPhone.bell();
+		dmbCellPhone.sendVoice("여보세요");
+		dmbCellPhone.receiveVoice("안녕하세요 저는 홍길동이예요.");
+		dmbCellPhone.sendVoice("아 네 반갑습니다.");
+		dmbCellPhone.hangUp();
+		
+		dmbCellPhone.turnOnDmb();
+		dmbCellPhone.changeChannelDmb(12);
+		dmbCellPhone.turnOffDmb();
+		
+	}
+
+}
