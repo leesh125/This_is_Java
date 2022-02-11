6811eebb420e6a461efc0c0ebee4c786f5a11e52
leesh125 <chelsea12345@naver.com>
2022-02-11 19:18:45 +0900

    11장 기본 API 클래스 - 11.16 java.time 패키지

---
+++ this_is_java/src/chap11/sec16/exam01_java_time/DateTimeOperationExample.java
@@ -0,0 +1,21 @@
+package chap11.sec16.exam01_java_time;
+
+import java.time.LocalDateTime;
+
+public class DateTimeOperationExample {
+	public static void main(String[] args) {
+		
+		LocalDateTime now = LocalDateTime.now();
+		System.out.println("현재 시간: " + now);
+		
+		LocalDateTime targetDateTime = now
+				.plusYears(1)
+				.minusMonths(2)
+				.plusDays(3)
+				.plusHours(4)
+				.minusMinutes(5)
+				.plusSeconds(6);
+		System.out.println("  연산 후: " + targetDateTime);
+		
+	}
+}
