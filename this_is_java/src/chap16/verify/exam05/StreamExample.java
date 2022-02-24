8495d423f056ad7fae25c5f8abb291f2ebe1bfc3
leesh125 <chelsea12345@naver.com>
2022-02-25 02:06:46 +0900

    16장 스트림과 병렬 처리 - 16장 확인문제

---
+++ this_is_java/src/chap16/verify/exam05/StreamExample.java
@@ -0,0 +1,22 @@
+package chap16.verify.exam05;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamExample {
+
+	public static void main(String[] args) {
+		List<String> list = Arrays.asList(
+			"This is a java book",
+			"Lambda Expressions",
+			"Java8 supprots lambda expressions"
+		);
+		
+		list.stream()
+			.filter(s -> s.toLowerCase().contains("java"))
+			.forEach(a -> System.out.println(a));
+				
+
+	}
+
+}
