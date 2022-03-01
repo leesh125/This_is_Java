c4dd661b9985dcea79d437c30b9905eeab31ab00
leesh125 <chelsea12345@naver.com>
2022-03-02 03:09:03 +0900

    17장 JavaFX - 17장 확인문제

---
+++ this_is_java/src/chap17/verify/exam01/RootController.java
@@ -0,0 +1,15 @@
+package chap17.verify.exam01;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class RootController implements Initializable{
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+}
