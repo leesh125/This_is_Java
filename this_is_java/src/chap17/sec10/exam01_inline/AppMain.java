eb024ec41113c8e118b57c183b586a84c626d9a0
leesh125 <chelsea12345@naver.com>
2022-03-01 20:08:49 +0900

    17장 JavaFX - 17.10 JavaFX CSS 스타일

---
+++ this_is_java/src/chap17/sec10/exam01_inline/AppMain.java
@@ -0,0 +1,31 @@
+package chap17.sec10.exam01_inline;
+
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class AppMain extends Application{
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Parent parent = FXMLLoader.load(getClass().getResource("root.fxml"));
+		Scene scene = new Scene(parent);
+		
+		primaryStage.setTitle("AppMain");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
+	}
+	
+	public static void main(String[] args) {
+		// Application이 갖고있는 정적 메소드
+		launch(args); 
+	}
+
+	
+
+}
