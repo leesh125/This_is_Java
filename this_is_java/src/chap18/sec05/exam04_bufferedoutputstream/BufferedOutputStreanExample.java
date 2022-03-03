2fad75673fe04271b1951f2ecbabefdc1cf0bc9c
leesh125 <chelsea12345@naver.com>
2022-03-03 19:28:38 +0900

    18장 IO 기반 입출력 및 네트워킹 - 18.5 보조 스트림

---
+++ this_is_java/src/chap18/sec05/exam04_bufferedoutputstream/BufferedOutputStreanExample.java
@@ -0,0 +1,47 @@
+package chap18.sec05.exam04_bufferedoutputstream;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+public class BufferedOutputStreanExample {
+
+	public static void main(String[] args) throws Exception {
+		FileInputStream fis = null;
+		FileOutputStream fos = null;
+		BufferedInputStream bis = null;
+		BufferedOutputStream bos = null;
+		
+		int data = -1;
+		long start = 0;
+		long end= 0;
+		
+		fis = new FileInputStream("C:\\Users\\leesh\\git\\This_is_Java\\this_is_java\\src\\chap18\\sec05\\exam04_bufferedreader\\11223659_969010503157601_4319241685982588557_n.jpg");
+        bis = new BufferedInputStream(fis);
+        fos = new FileOutputStream("C:/Temp/foret.jpg");
+        start = System.currentTimeMillis();
+        while((data=bis.read()) != -1) {
+        	fos.write(data);
+        }
+        fos.flush();
+        end = System.currentTimeMillis();
+        fos.close(); bis.close(); fis.close();
+        System.out.println("사용하지 않았을 때: " + (end-start) + "ms");
+        
+        fis = new FileInputStream("C:\\Users\\leesh\\git\\This_is_Java\\this_is_java\\src\\chap18\\sec05\\exam04_bufferedreader\\11223659_969010503157601_4319241685982588557_n.jpg");
+        bis = new BufferedInputStream(fis);
+        fos = new FileOutputStream("C:/Temp/foret.jpg");
+        bos = new BufferedOutputStream(fos);
+        start = System.currentTimeMillis();
+        while((data=bis.read()) != -1) {
+        	bos.write(data);
+        }
+        fos.flush();
+        end = System.currentTimeMillis();
+        bos.close(); bis.close(); fis.close();
+        System.out.println("사용했을 때: " + (end-start) + "ms");
+	}
+
+}
