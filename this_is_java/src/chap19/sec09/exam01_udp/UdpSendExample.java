926befe7f612257ee2513f05c3eadef7e9ab4596
leesh125 <chelsea12345@naver.com>
2022-03-10 03:21:29 +0900

    19장 NIO 기반 입출력 및 네트워킹 - 19.9 UDP 채널

---
+++ this_is_java/src/chap19/sec09/exam01_udp/UdpSendExample.java
@@ -0,0 +1,33 @@
+package chap19.sec09.exam01_udp;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.StandardProtocolFamily;
+import java.nio.ByteBuffer;
+import java.nio.channels.DatagramChannel;
+import java.nio.charset.Charset;
+
+public class UdpSendExample {
+
+	public static void main(String[] args) throws IOException {
+		DatagramChannel datagramChannel = DatagramChannel.open(StandardProtocolFamily.INET);
+		
+		System.out.println("[발신 시작]");
+		
+		for(int i=1;i<=3; i++) {
+			String data  = "메시지 " + i;
+			Charset charset = Charset.forName("UTF-8");
+			ByteBuffer byteBuffer = charset.encode(data);
+			
+			int byteCount = datagramChannel.send(
+				byteBuffer, 
+				new InetSocketAddress("localhost", 5001)
+			);
+			System.out.println("[보낸 바이트 수] " + byteCount +  " bytes");
+		}
+
+		System.out.println("[발신 종료]");
+		datagramChannel.close();
+	}
+
+}
